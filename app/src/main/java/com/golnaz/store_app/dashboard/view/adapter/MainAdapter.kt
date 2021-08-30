0126d053b6b67c430a2a3b81372c46fec3af77fa
golnaz <g.torabi76@gmail.com>
2021-08-30 22:11:55 +0430

    fix some ui files

--- app/src/main/java/com/golnaz/store_app/dashboard/view/adapter/MainAdapter.kt
+++ app/src/main/java/com/golnaz/store_app/dashboard/view/adapter/MainAdapter.kt
@@ -7,12 +7,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.golnaz.store_app.R
 import com.golnaz.store_app.dashboard.domain.model.AppData
 import com.golnaz.store_app.databinding.HeaderItemListBinding
+import com.golnaz.store_app.databinding.MainItemListBinding
 import javax.inject.Inject
 
 class MainAdapter @Inject constructor(): RecyclerView.Adapter<MainAdapter.MainHolder>() {
      var items: MutableList<AppData> = mutableListOf()
 
-    inner class MainHolder(private val binding: HeaderItemListBinding) :
+    inner class MainHolder(private val binding: MainItemListBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(model: AppData) {
             binding.appData = model
@@ -24,8 +25,8 @@ class MainAdapter @Inject constructor(): RecyclerView.Adapter<MainAdapter.MainHo
         if (inflater == null) {
             inflater = LayoutInflater.from(parent.context)
         }
-        val binding: HeaderItemListBinding =
-            DataBindingUtil.inflate(inflater!!, R.layout.header_item_list, parent, false)
+        val binding: MainItemListBinding =
+            DataBindingUtil.inflate(inflater!!, R.layout.main_item_list, parent, false)
         return MainHolder(binding)
     }
 
--- app/src/test/java/com/golnaz/store_app/dashboard/view/adapter/MainAdapterTest.kt
+++ app/src/test/java/com/golnaz/store_app/dashboard/view/adapter/MainAdapterTest.kt
@@ -1,12 +1,14 @@
 package com.golnaz.store_app.dashboard.view.adapter
 
+import androidx.recyclerview.widget.RecyclerView
 import com.golnaz.store_app.dashboard.domain.model.AppData
 import com.nhaarman.mockito_kotlin.spy
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.Assertions
-import org.mockito.Mock
+import org.mockito.InjectMocks
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
@@ -14,8 +16,9 @@ import org.mockito.junit.MockitoRule
 
 class MainAdapterTest {
     private lateinit var items: MutableList<AppData>
+    lateinit var recyclerView : RecyclerView
 
-    @Mock
+    @InjectMocks
     lateinit var mainAdapter: MainAdapter
 
     @get:Rule
@@ -24,6 +27,7 @@ class MainAdapterTest {
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
+        recyclerView = Mockito.mock(RecyclerView::class.java)
         items = mutableListOf()
         items.add(0, AppData(appImage = "AIcon", appName = "A", appRate = "1"))
         items.add(1, AppData(appImage = "BIcon", appName = "B", appRate = "2"))
@@ -31,9 +35,6 @@ class MainAdapterTest {
         items.add(3, AppData(appImage = "DIcon", appName = "D", appRate = "4.4"))
         items.add(4, AppData(appImage = "EIcon", appName = "E", appRate = "1.5"))
         mainAdapter = spy(MainAdapter())
-        /*doNothing().`when`(mainAdapter as RecyclerView.Adapter<*>?)?.notifyItemInserted(
-            anyInt()
-        )*/
 
     }
 
